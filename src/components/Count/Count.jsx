14fd5c36b5b9151ed77c27b0d4d9ad929d2a4749
walid hasan <walid.official8@gmail.com>
2024-12-22 09:17:19 +0600

    new section is added

---
+++ src/components/Count/Count.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
+
+const Count = () => {
+  const [counterOn, setCounterOn] = useState(false);
+  return (
+    <div className="w-11/12 mx-auto my-10">
+      <div className="flex py-8 gap-3 justify-center items-center">
+        <div className="bg-[#8e67f1] w-20 h-1"></div>
+        <h3 className="text-center font-bold text-3xl">Learning Journey Highlights</h3>
+        <div className="bg-[#8e67f1] w-20 h-1"></div>
+      </div>
+      <div className="flex gap-8">
+        <div className="w-[50%]">
+          <img
+            src="https://media.istockphoto.com/id/1482901847/vector/adobe-illustrator-artwork.jpg?s=612x612&w=0&k=20&c=Z-ovrzaxDTqvAtRQ7T56NCmF9-o6wWkTVVDktH9cd-A="
+            alt=""
+          />
+        </div>
+        <div className="">
+          <ScrollTrigger
+            onEnter={() => setCounterOn(true)}
+            onExit={() => setCounterOn(false)}
+          >
+            <div className="grid md:grid-cols-2 py-20 gap-8 justify-between">
+              {/* Counter 1 */}
+              <div className="text-center border p-3 text-[#5e3598]">
+                <div className=" text-6xl font-bold w-full">
+                  {counterOn && <CountUp end={58.6} />}K
+                </div>
+                <h2 className="text-center py-3 font-bold text-xl text-[#0b0505]">
+                  Student enrolled
+                </h2>
+                <p className="text-center text-[#0b0505]">
+                  Successfully registered and ready to access learning
+                  materials.
+                </p>
+              </div>
+              {/* Counter 2 */}
+              <div className="text-center border p-3 text-[#5e3598]">
+                <div className=" text-6xl font-bold">
+                  {counterOn && <CountUp end={38.6} />}K
+                </div>
+                <h2 className="text-center py-3 font-bold text-xl text-[#0b0505]">
+                  Class completed
+                </h2>
+                <p className="text-center text-[#0b0505]">
+                  Completed the session with new skills gained.
+                </p>
+              </div>
+              {/* Counter 3 */}
+              <div className="text-center border p-3 text-[#5e3598]">
+                <div className=" text-6xl font-bold">
+                  {counterOn && <CountUp end={98.9} />}%
+                </div>
+                <h2 className="text-center py-3 font-bold text-xl text-[#0b0505]">
+                  Satisfaction rate
+                </h2>
+                <p className="text-center text-[#0b0505]">
+                  Reflects student happiness and service quality.
+                </p>
+              </div>
+              {/* Counter 4 */}
+              <div className="text-center border p-3 text-[#5e3598]">
+                <div className=" text-6xl font-bold">
+                  {counterOn && <CountUp end={236} />}K
+                </div>
+                <h2 className="text-center py-3 font-bold text-xl text-[#0b0505]">
+                  Top instructors
+                </h2>
+                <p className="text-center text-[#0b0505]">
+                  Highly rated experts with proven excellence.
+                </p>
+              </div>
+            </div>
+          </ScrollTrigger>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Count;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import Question from '../components/Questions/Question';
+import Count from '../components/Count/Count';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <Count></Count>
+            <Question></Question>
         </div>
     );
 };
