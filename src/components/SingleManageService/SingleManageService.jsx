5857b6f03244d5aa5ce381a49972212e849a9346
walid hasan <walid.official8@gmail.com>
2024-12-22 23:33:00 +0600

    delete operation is added

--- src/components/SingleManageService/SingleManageService.jsx
+++ src/components/SingleManageService/SingleManageService.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
-const SingleManageService = () => {
-    return (
-        <div>
-            
+const SingleManageService = ({ manageService,handleDeleteService }) => {
+  const {
+    _id,
+    serviceName,
+    description,
+    servicePhoto,
+    serviceArea,
+    serviceProviderData,
+    price,
+  } = manageService || {};
+
+
+
+
+  return (
+    <div className="w-[70%] mx-auto my-10">
+      <div className="">
+        <div className="grid md:grid-cols-2 p-4 bg-base-100 shadow-xl">
+          <figure className="flex justify-center items-center">
+            <img
+                className="rounded-full w-[250px]  h-[250px] object-cover border-[#8e67f1] border-b-4"
+              src={servicePhoto}
+              alt="Movie"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="text-2xl font-extrabold">{serviceName}</h2>
+            <h2 className="font-bold">{serviceArea}</h2>
+            <p>{description.slice(0,120)}...</p>
+            <div className="card-actions justify-start">
+              <button onClick={() => handleDeleteService(_id)} className="btn bg-gradient-to-r from-[#8e67f1] to-[#8e67f1ae] text-white">Delete</button>
+              <button className="btn bg-gradient-to-r from-[#8e67f1] to-[#8e67f1ae] text-white">Update</button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SingleManageService;
--- src/pages/ManageService.jsx
+++ src/pages/ManageService.jsx
@@ -1,31 +1,68 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../components/AuthProvider/AuthProvider';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../components/AuthProvider/AuthProvider";
+import axios from "axios";
+import SingleManageService from "../components/SingleManageService/SingleManageService";
+import Swal from "sweetalert2";
 
 const ManageService = () => {
-    const {user} = useContext(AuthContext);
-    console.log(user?.email);
+  const { user } = useContext(AuthContext);
+  console.log(user?.email);
 
-    const [manageServices, setManageServices] = useState([]);
-    useEffect(() => {
-        const fetchServices = async () => {
-            try {
-                const { data } = await axios.get(
-                    `${import.meta.env.VITE_API_URL}/services/${user?.email}`
-                );
-                setManageServices(data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchServices();
-    }, [user])
-    console.log(manageServices);
-    return (
-        <div>
-            
-        </div>
-    );
+  const [manageServices, setManageServices] = useState([]);
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const { data } = await axios.get(
+          `${import.meta.env.VITE_API_URL}/services/${user?.email}`
+        );
+        setManageServices(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchServices();
+  }, [user]);
+
+  const handleDeleteService = async (id) => {
+    try {
+      const { data } = await axios.delete(
+        `${import.meta.env.VITE_API_URL}/deleteService/${id}`
+      );
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        }
+        const remainingServices = manageServices.filter(
+          (service) => service._id !== id
+        );
+        setManageServices(remainingServices);
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(manageServices);
+  return (
+    <div>
+      {manageServices.map((manageService, index) => (
+        <SingleManageService
+          key={index}
+          handleDeleteService={handleDeleteService}
+          manageService={manageService}
+        ></SingleManageService>
+      ))}
+    </div>
+  );
 };
 
 export default ManageService;
