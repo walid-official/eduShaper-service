e70286396f55fe811b02802110d02fd2bdcd0c36
walid hasan <walid.official8@gmail.com>
2025-01-06 21:35:27 +0600

    contact functionality is added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { FaPhoneVolume } from "react-icons/fa6";
 import { SiMinutemailer } from "react-icons/si";
 import { LiaAddressCardSolid } from "react-icons/lia";
 import mail from "../assets/images/mail2.gif"
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
+
 const Contact = () => {
+
+    const form = useRef();
+    const sendEmail = (e) => {
+      e.preventDefault();
+      emailjs
+        .sendForm(
+          "service_zkobzwh",
+          "template_kvl3fu6",
+          form.current,
+          "n9eOVgGeK1V5CpTT0"
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.text);
+            toast.success("Email sent successfully!");
+            form.current.reset(); 
+          },
+          (error) => {
+            console.error("FAILED...", error.text);
+            alert("Failed to send the email. Please try again later.");
+          }
+        );
+    };
+
+
   return (
     <div>
-      <div className="bg-[#f2f2f2] py-10">
+      <div className="bg-[#f2f2f2] py-12">
         <h2 className="font-bold text-center text-4xl">Contact Us</h2>
 
         <div className="flex justify-center py-3 gap-2 ">
@@ -22,11 +50,11 @@ const Contact = () => {
         </div>
       </div>
       <div className="w-11/12 mx-auto  pt-10">
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2  gap-4">
           <div className="card border rounded-none bg-base-100  shadow-xl hover:shadow-lg duration-300">
             <div className=" py-6 flex gap-5">
               <div className="">
-                <h2 className="text-white p-5 bg-[#8638d9d4] text-3xl">
+                <h2 className="text-white p-5 bg-gradient-to-r from-[#774ede] to-[#8e67f1cb] text-3xl">
                   <FaPhoneVolume></FaPhoneVolume>
                 </h2>
               </div>
@@ -43,7 +71,7 @@ const Contact = () => {
           <div className="card border rounded-none bg-base-100  shadow-xl hover:shadow-lg duration-300">
             <div className=" py-6 flex gap-5">
               <div className="">
-                <h2 className="text-white p-5 bg-[#8638d9d4] text-3xl">
+                <h2 className="text-white p-5 bg-gradient-to-r from-[#774ede] to-[#8e67f1cb] text-3xl">
                   <SiMinutemailer></SiMinutemailer>
                 </h2>
               </div>
@@ -57,10 +85,10 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <div className="card border rounded-none bg-base-100  shadow-xl hover:shadow-lg duration-300">
+          <div className="card border pr-3 rounded-none bg-base-100  shadow-xl hover:shadow-lg duration-300">
             <div className=" py-6 flex gap-5">
               <div className="">
-                <h2 className="text-white p-4 bg-[#8638d9d4] text-4xl">
+                <h2 className="text-white p-4 bg-gradient-to-r from-[#774ede] to-[#8e67f1cb] text-4xl">
                   <LiaAddressCardSolid ></LiaAddressCardSolid>
                 </h2>
               </div>
@@ -75,12 +103,58 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <div className="">
-        <div className="">
-            <div className="">
+      <div className="w-11/12 mx-auto py-14">
+        <div className="lg:flex ">
+            <div className="lg:w-[60%]">
                 <img src={mail} alt="" />
             </div>
-            <div className=""></div>
+            <div className="lg:w-[40%]">
+            <div className="card w-full shadow-2xl">
+            <form ref={form} onSubmit={sendEmail}  className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black">Name</span>
+                </label>
+                <input
+                  type="name"
+                  placeholder="Name"
+                  name="from_name"
+                  className="border  p-4 rounded-md"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="from_name"
+                  placeholder="Email"
+                  className="border  p-4 rounded-md"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black">Message</span>
+                </label>
+                <textarea
+                  type="text"
+                  name="message"
+                  placeholder="Message"
+                  className="border  p-6 pb-14 rounded-md"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="md:w-[40%] bg-gradient-to-r from-[#774ede] to-[#8e67f1cb] font-bold text-white px-10 py-4 rounded-full shadow-lg border">
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+            </div>
         </div>
       </div>
     </div>
